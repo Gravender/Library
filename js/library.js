d338d5a7a44fbb83e9f155dfa93a89888db39c01
Gravender <ss.noah@gmail.com>
2021-01-30 07:30:20 +1100

    Updated Stlying and Added Clarity to Html

--- js/library.js
+++ js/library.js
@@ -37,6 +37,8 @@ function updateLibrary(){
         let title = document.createElement("div");
         let author = document.createElement("div");
         let pages = document.createElement("div");
+        let choiceDiv = document.createElement("div");
+        let readDiv = document.createElement("div");
         let read = document.createElement("input");
         let readLabel = document.createElement("LABEL");
         let deleteBook = document.createElement("button");
@@ -53,10 +55,15 @@ function updateLibrary(){
         pages.innerText = `${Number(element.pages)} pages`;
         pages.classList.add('pages');
         
+        choiceDiv.classList.add('choiceDiv');
+
+        readDiv.classList.add('readDiv');
+
         readLabel.setAttribute("for", "read");
         readLabel.textContent = 'Read';
         read.setAttribute('type', 'checkbox');
         read.classList.add('read');
+        read.classList.add('checkBook');
         if(element.read){
             read.setAttribute('checked', '');
             book.style.border = "3px solid teal";
@@ -83,12 +90,46 @@ function updateLibrary(){
         book.appendChild(title);
         book.appendChild(author);
         book.appendChild(pages);
-        book.appendChild(readLabel);
-        book.appendChild(read);
-        book.appendChild(deleteBook);
+        readDiv.appendChild(readLabel);
+        readDiv.appendChild(read);
+        choiceDiv.appendChild(readDiv);
+        choiceDiv.appendChild(deleteBook);
+        book.appendChild(choiceDiv);
 
         container.appendChild(book).className = "book";
     });
+}
+
+function storageAvailable(type) {
+    var storage;
+    try {
+        storage = window[type];
+        var x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    }
+    catch(e) {
+        return e instanceof DOMException && (
+            // everything except Firefox
+            e.code === 22 ||
+            // Firefox
+            e.code === 1014 ||
+            // test name field too, because code might not be present
+            // everything except Firefox
+            e.name === 'QuotaExceededError' ||
+            // Firefox
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            (storage && storage.length !== 0);
+    }
+}
+
+if (storageAvailable('localStorage')) {
+    // Yippee! We can use localStorage awesomeness
+}
+else {
+    // Too bad, no localStorage for us
 }
 myLibrary.push(new Book('The Hobbit', 'J.R.R. Tolkien', 295, false));
 myLibrary.push(new Book('The Lord Of the Rings', 'J.R.R. Tolkien', 900, true));
